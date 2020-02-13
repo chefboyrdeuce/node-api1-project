6ec93de25439b36cc0a73133ca41e5aa9c359cc4
chefboyrdeuce <jpldeuce@gmail.com>
2020-02-12 22:14:43 -0500

    installed npm, express, created post endpoints

--- data/db.js
+++ data/db.js
@@ -10,28 +10,33 @@ module.exports = {
   remove,
 };
 
+//returns a promise that resolves to an array of all the users contained in the database
 function find() {
   return db('users');
 }
 
+//expects an id as parameter and returns the users corresponding to the id provided or an empty array if no user with that id is found
 function findById(id) {
   return db('users')
     .where({ id: Number(id) })
     .first();
 }
 
+//expects a user object and will add it to the database and return an object with the id of the inserted user
 function insert(user) {
   return db('users')
     .insert(user)
     .then(ids => ({ id: ids[0] }));
 }
 
+//accepts the id of the user and a user object with the changes to apply.  Returns the count of updated records.  If the count is 1 it means the record was updated correctly.
 function update(id, user) {
   return db('users')
     .where('id', Number(id))
     .update(user);
 }
 
+//accepts id as a parameter and upon successfuly deleting the user from the database it returns the number of records deleted
 function remove(id) {
   return db('users')
     .where('id', Number(id))
--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // implement your API here
+
+const express = require('express')
+const server = express()
+
+server.get("/", (req, res) => {
+    res.json({ message: "hello, world" })
+})
+
+server.post("/api/users", (req, res) => {
+const newUser = {
+    name: "",
+    bio: "",
+}
+
+users.push(newUser)
+if (user) {
+    users.push(newUser)
+    res.status(201).json(newUser)
+} else {
+    res.status(400).json({ errorMessage: "Please provide name and bio for the user" })
+}
+})
+
+server.get("api/users/:id", (req,res) => {
+    const id = req.params.id
+    const user = users.find(u => u.id == id)
+
+    if (user) {
+        res.json(user)
+    } else {
+        res.status(404).json({ message: "The user with the specified ID does not exist" })
+    }
+})
+
+const port = 8080
+
+server.listen(port, () => {
+console.log(`server started at http://localhost:${port}`)
+})
+
+
+// server.get("/users/:id", (req,res) => {
+//   const id = req.params.id
+//   const user = users.find(u => u.id == id)
+
+//   if (user) {
+//       res.json(user)
+//   } else {
+//       res.status(404).json({ message: "User not found" })
+//   }
+// })
